1aad00e5ee58e909065a50ae60fcdbe403b7c24f
senritsu <teh.b4k4@gmail.com>
2016-10-30 15:34:16 +0100

    .sm format tests

---
+++ SharpRhythms.Tests/Formats/SmTests.cs
@@ -0,0 +1,87 @@
+﻿/***************************************************************************\
+The MIT License (MIT)
+
+Copyright (c) 2016 senritsu (https://github.com/senritsu)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+\***************************************************************************/
+
+namespace SharpRhythms.Tests.Formats
+{
+    using System;
+    using System.Linq;
+    using Enums;
+    using Parsers.SM;
+    using Shouldly;
+    using Xunit;
+
+    public class SmTests
+    {
+        [Fact]
+        public void BpmChangesAreParsedCorrectly()
+        {
+            var parser = new SmParser();
+            var actual = parser.LoadFromFile("files/SM/bpm_changes_and_stops.sm").Tempo.Bpm.ToArray();
+            actual.Length.ShouldBe(3);
+            actual.Select(x => x.Time).ShouldBe(new [] {1.0, 3.0, 5.0});
+            actual.Select(x => x.Bpm).ShouldBe(new [] {2.0, 4.0, 6.0});
+        }
+
+        [Fact]
+        public void StopsAreParsedCorrectly()
+        {
+            var parser = new SmParser();
+            var actual = parser.LoadFromFile("files/SM/bpm_changes_and_stops.sm").Tempo.Interruptions.ToArray();
+            actual.Length.ShouldBe(3);
+            actual.Select(x => x.Time).ShouldBe(new[] { 1.0, 3.0, 5.0 });
+            actual.Select(x => x.Duration).ShouldBe(new[] { 2.0, 4.0, 6.0 });
+        }
+
+        public void NotesAreParsedCorrectly()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Fact]
+        public void ChartsAreParsedCorrectly()
+        {
+            var parser = new SmParser();
+            var actual = parser.LoadFromFile("files/SM/chart_definitions.sm").Charts;
+            actual.Count.ShouldBe(2);
+            actual.First().Type.ShouldBe(StepChartType.Single);
+            actual.First().DescriptionOrAuthor.ShouldBe("single chart");
+            actual.First().DifficultyRating.Difficulty.ShouldBe(Difficulty.Easy);
+            actual.First().DifficultyRating.Meter.ShouldBe(1);
+            actual.First().DifficultyRating.Radar.Stream.ShouldBe(0.1);
+            actual.First().DifficultyRating.Radar.Voltage.ShouldBe(0.2);
+            actual.First().DifficultyRating.Radar.Air.ShouldBe(0.3);
+            actual.First().DifficultyRating.Radar.Freeze.ShouldBe(0.4);
+            actual.First().DifficultyRating.Radar.Chaos.ShouldBe(0.5);
+            actual.Last().Type.ShouldBe(StepChartType.Double);
+            actual.Last().DescriptionOrAuthor.ShouldBe("double chart");
+            actual.Last().DifficultyRating.Difficulty.ShouldBe(Difficulty.Medium);
+            actual.Last().DifficultyRating.Meter.ShouldBe(2);
+            actual.Last().DifficultyRating.Radar.Stream.ShouldBe(0.5);
+            actual.Last().DifficultyRating.Radar.Voltage.ShouldBe(0.4);
+            actual.Last().DifficultyRating.Radar.Air.ShouldBe(0.3);
+            actual.Last().DifficultyRating.Radar.Freeze.ShouldBe(0.2);
+            actual.Last().DifficultyRating.Radar.Chaos.ShouldBe(0.1);
+        }
+    }
+}
---
+++ SharpRhythms.Tests/IntegrationTests/SimpleTestSong.cs
@@ -0,0 +1,121 @@
+﻿/***************************************************************************\
+The MIT License (MIT)
+
+Copyright (c) 2016 senritsu (https://github.com/senritsu)
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+\***************************************************************************/
+
+namespace SharpRhythms.Tests.IntegrationTests
+{
+    using System.Linq;
+    using Enums;
+    using Parsers.SM;
+    using Shouldly;
+    using Xunit;
+
+    public class SimpleTestSong
+    {
+        [Fact]
+        public void LoadsCorrectlyFromSMFormat()
+        {
+            var track = new SmParser().LoadFromFile("files/SM/simple_test_song.sm");
+
+            track.Information.Title.Original.ShouldBe("作品名");
+            track.Information.Title.Alternative.ShouldBe("Sakuhinmei");
+            track.Information.Subtitle.Original.ShouldBe("傍題");
+            track.Information.Subtitle.Alternative.ShouldBe("Boudai");
+            track.Information.Artist.Original.ShouldBe("作者");
+            track.Information.Artist.Alternative.ShouldBe("Sakusha");
+            track.Information.Selectable.ShouldBe(true);
+            track.Information.Credit.ShouldBe("senritsu");
+            track.Information.DisplayBpm.Min.ShouldBe(180);
+            track.Information.DisplayBpm.Mode.ShouldBe(BpmDisplayMode.Static);
+            track.Information.Genre.ShouldBe("Test Genre");
+            track.Information.New.ShouldBe(false);
+
+            track.Tempo.Bpm.Count().ShouldBe(2);
+            track.Tempo.Bpm.First().Time.ShouldBe(0);
+            track.Tempo.Bpm.First().Bpm.ShouldBe(120);
+            track.Tempo.Bpm.Last().Time.ShouldBe(60);
+            track.Tempo.Bpm.Last().Bpm.ShouldBe(180);
+            track.Tempo.Interruptions.ShouldHaveSingleItem();
+            track.Tempo.Interruptions.Single().Time.ShouldBe(120);
+            track.Tempo.Interruptions.Single().Duration.ShouldBe(1);
+
+            track.Files.Background.ShouldBe("bg.png");
+            track.Files.Banner.ShouldBe("banner.png");
+            track.Files.CdTitleIcon.ShouldBe("icon.png");
+            track.Files.Lyrics.ShouldBe("test.lrc");
+            track.Files.Music.ShouldBe("test.mp3");
+            track.Files.MusicMd5.ShouldBe(null);
+
+            track.Preview.SampleStart.ShouldBe(30);
+            track.Preview.SampleLength.ShouldBe(10);
+
+            track.Charts.ShouldHaveSingleItem();
+
+            var chart = track.Charts.Single();
+
+            chart.DescriptionOrAuthor.ShouldBe("description");
+            chart.DifficultyRating.Difficulty.ShouldBe(Difficulty.Easy);
+            chart.DifficultyRating.Meter.ShouldBe(1);
+            chart.DifficultyRating.Radar.Stream.ShouldBe(0.1);
+            chart.DifficultyRating.Radar.Voltage.ShouldBe(0.2);
+            chart.DifficultyRating.Radar.Air.ShouldBe(0.3);
+            chart.DifficultyRating.Radar.Freeze.ShouldBe(0.4);
+            chart.DifficultyRating.Radar.Chaos.ShouldBe(0.5);
+
+            chart.Type.ShouldBe(StepChartType.Single);
+            chart.Measures.Count().ShouldBe(4);
+            chart.Measures.ShouldAllBe(x => x.TimeSignature.BeatValue == 4);
+            chart.Measures.ShouldAllBe(x => x.TimeSignature.Beats == 4);
+            chart.Measures.ShouldAllBe(x => x.Rows.Length == 4);
+
+            var notes = chart.Notes.ToArray();
+            notes.Length.ShouldBe(16);
+            notes.ShouldAllBe(x => x.Type == StepType.Normal);
+            notes.ShouldAllBe(x => x.Numerator == 1);
+            notes.ShouldAllBe(x => x.Denominator == 4);
+            notes.Select(x => x.Direction).ShouldBe(new []
+            {
+                Direction.Left,
+                Direction.Down,
+                Direction.Up, 
+                Direction.Right, 
+                Direction.Left, 
+                Direction.Left, 
+                Direction.Left, 
+                Direction.Left, 
+                Direction.Down, 
+                Direction.Right, 
+                Direction.Down, 
+                Direction.Right, 
+                Direction.Left, 
+                Direction.Up, 
+                Direction.Left, 
+                Direction.Up
+            });
+            //notes.Select(x => x.Time.ShouldBe(new []
+            //{
+            //    1.234, 
+            //}))
+        }
+    }
+}
